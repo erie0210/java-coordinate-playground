8f5683fe6389483ffbfc6deb4aa65472a5f66cda
erie0210 <eriee0210@gmail.com>
2021-12-02 16:54:19 +0900

    test: 테스트 케이스 추가

---
+++ src/test/java/FigureFactoryTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FigureFactoryTest {
+    @Test
+    public void line() {
+        List<Point> points = Arrays.asList(
+                Point.of(1, 2),
+                Point.of(2, 3));
+
+        AbstractFigure abstractFigure = FigureFactory.getInstance(points); // static factory method
+        assertThat(abstractFigure).isInstanceOfAny(Line.class);
+        assertThat(abstractFigure.getName()).isEqualTo("선");
+    }
+
+    @Test
+    public void triangle() {
+        List<Point> points = Arrays.asList(
+                Point.of(1, 1),
+                Point.of(4, 1),
+                Point.of(1, 4));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOfAny(Triangle.class);
+        assertThat(figure.getName()).isEqualTo("삼각형");
+    }
+
+    @Test
+    public void rectangle() {
+        List<Point> points = Arrays.asList(
+                Point.of(1, 1),
+                Point.of(4, 1),
+                Point.of(1, 4),
+                Point.of(4, 4));
+
+        Figure figure = FigureFactory.getInstance(points);
+        assertThat(figure).isInstanceOfAny(Rectangle.class);
+        assertThat(figure.getName()).isEqualTo("사각형");
+    }
+}
