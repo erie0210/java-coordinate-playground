a1456f80cf02bb0a4cf3719d3a3893fb4e8525d3
erie0210 <eriee0210@gmail.com>
2021-12-02 17:00:20 +0900

    feat: message를 한 곳에 구현

--- src/main/java/SystemMessage.java
+++ src/main/java/SystemMessage.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class SystemMessage {
+public class SystemMessage {
+    public static final int LINE_POINT_SIZE=2;
+    public static final int TRIANGLE_POINT_SIZE=2;
+    public static final int RECTANGLE_POINT_SIZE=2;
+
+    public static final String LINE_NAME = "선";
+    public static final String TRIANGLE_NAME = "삼각형";
+    public static final String RECTANGLE_NAME = "사각형";
 }
