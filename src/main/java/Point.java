8309ed131ee2f23ef599b5c24b07378c41f5a77e
erie0210 <eriee0210@gmail.com>
2021-12-02 16:59:13 +0900

    feat: 점 기능 구현

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private final int x;
+    private final int y;
+
+    private Point(int x, int y){
+        this.x = x;
+        if(x < 0 || x > 24){
+            throw new IllegalArgumentException();
+        }
+        this.y = y;
+        if(y < 0 || y > 24){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public double getDistance(Point other){
+        return Math.sqrt((x-other.x)*(x-other.x)) + Math.sqrt((y-other.y)*(y-other.y));
+    };
+
+    public static Point of(int x, int y){
+        return new Point(x, y);
+    }
 }
